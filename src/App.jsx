ab4d3b11d559e931224b770fb45755d6b9da6446
varad-n <hvi924926@gmail.com>
2025-05-06 20:32:28 +0530

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import "./App.css"
+
 const App = () => {
-  return <div>Hello WOrld!</div>;
+  const  [username, setUsername] = useState("")
+  const  [user, setUser] = useState("")
+
+  return <div className="container">
+  <div className="login">
+    <input type="text" placeholder="username" onChange={(e)=>setUsername(e.target.value)} />
+    <button>Login</button>
+  </div>
+
+  </div>;
 };
 
 export default App;
